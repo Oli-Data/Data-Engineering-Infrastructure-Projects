691a8729edf01abce9cc862c86314662ccf06e4b
Christian Olivares-Rodriguez <69546302+Oli-Data@users.noreply.github.com>
2025-08-29 20:27:44 -0700

    Delete Chicago Schools Project directory

--- Chicago Schools Project/Project1ChristianOlivarres-Rodriguez.R
+++
@@ -1,8 +0,0 @@
-install.packages("dplyr")
-install.packages("ggplot2")
-
-df = read.csv("/Users/christianolivares/R Files/Chicago_Public_Schools_-_Progress_Report_Cards__2011-2012_.csv", header = TRUE)
-cat(names(df), sep = "\n")
-View(df)
-
-
